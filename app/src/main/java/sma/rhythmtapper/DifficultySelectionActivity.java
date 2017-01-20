9d3f57e18630fe4a5887835b714bbf6f77022339
Peter Schmidbauer <peter.schmidb@gmail.com>
2017-01-20 11:10:13 +0100

    main menu initial layout and buttons

---
+++ app/src/main/java/sma/rhythmtapper/DifficultySelectionActivity.java
@@ -0,0 +1,13 @@
+package sma.rhythmtapper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DifficultySelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_difficulty_selection);
+    }
+}
