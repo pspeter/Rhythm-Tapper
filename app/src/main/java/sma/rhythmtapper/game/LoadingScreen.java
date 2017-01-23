3ca5872d5f92dae1aeaf3381a9ec4c17314b3d07
bloody-orange <peter.schmidb@gmail.com>
2017-01-23 23:42:00 +0100

    new placeholder assets in correct size

--- app/src/main/java/sma/rhythmtapper/game/LoadingScreen.java
+++ app/src/main/java/sma/rhythmtapper/game/LoadingScreen.java
@@ -18,7 +18,7 @@ public class LoadingScreen extends Screen {
     public void update(float deltaTime) {
         Graphics g = game.getGraphics();
         Assets.background = g.newImage("background_placeholder.jpg", Graphics.ImageFormat.RGB565);
-        Assets.ball = g.newImage("ball_placeholder.jpg", Graphics.ImageFormat.RGB565);
+        Assets.ball = g.newImage("ball_placeholder.png", Graphics.ImageFormat.RGB565);
         Assets.click = game.getAudio().createSound("guiclick.ogg");
         game.setScreen(new GameScreen(game));
     }
