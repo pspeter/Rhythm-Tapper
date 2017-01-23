d91c935bf379248248da5b5b70a5b07c5238d750
bloody-orange <peter.schmidb@gmail.com>
2017-01-23 23:43:02 +0100

    balls spawning and moving balls despawn on touch or when leaving screen

--- app/src/main/java/sma/rhythmtapper/game/GameScreen.java
+++ app/src/main/java/sma/rhythmtapper/game/GameScreen.java
@@ -1,9 +1,13 @@
 package sma.rhythmtapper.game;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.Log;
 
 import sma.rhythmtapper.framework.Game;
@@ -19,19 +23,32 @@ public class GameScreen extends Screen {
     }
     private int _gameHeight;
     private int _gameWidth;
+    private int _score;
+    private int _multiplier;
+    private int _streak;
+    private List<Point> _points;
+    private Random _rand;
+    private int _tick;
 
     GameState state = GameState.Ready;
 
     // Variable Setup
     // You would create game objects here.
 
-    int livesLeft = 1;
-    Paint paint;
+    private Paint paint;
 
     public GameScreen(Game game) {
         super(game);
 
-        // Initialize game objects here
+        // Initialize game objects
+        _gameHeight = game.getGraphics().getHeight();
+        _gameWidth = game.getGraphics().getWidth();
+        _multiplier = 1;
+        _score = 0;
+        _streak = 0;
+        _points = new ArrayList<>();
+        _rand = new Random(42);
+        _tick = 0;
 
         // Defining a paint object
         paint = new Paint();
@@ -39,10 +56,6 @@ public class GameScreen extends Screen {
         paint.setTextAlign(Paint.Align.CENTER);
         paint.setAntiAlias(true);
         paint.setColor(Color.WHITE);
-        _gameHeight = game.getGraphics().getHeight();
-        _gameWidth = game.getGraphics().getWidth();
-        game.getGraphics().drawString("helloo", 30, 30, paint);
-        Log.d(TAG, "ctor gamescreen");
     }
 
     @Override
@@ -78,6 +91,8 @@ public class GameScreen extends Screen {
     private void updateRunning(List<TouchEvent> touchEvents, float deltaTime) {
 
         //This is identical to the update() method from our Unit 2/3 game.
+        spawnPoints();
+        movePoints();
 
 
         // 1. All touch input is handled here:
@@ -87,33 +102,80 @@ public class GameScreen extends Screen {
             TouchEvent event = touchEvents.get(i);
 
             if (event.type == TouchEvent.TOUCH_DOWN) {
-
-                Log.d(TAG, "x: " + event.x);
                 if (event.x < _gameWidth / 3) {
-
-                    Log.d(TAG, "left");
+                    hitLane(0, _gameWidth / 3);
                 }
-
                 else if (event.x < _gameWidth / 3 * 2) {
-                    Log.d(TAG, "middle");
+                    hitLane(_gameWidth / 3, _gameWidth / 3 * 2);
                 }
                 else {
-                    Log.d(TAG, "right");
+                    hitLane(_gameWidth / 3 * 2, _gameWidth);
                 }
+            }
+        }
 
+        Iterator<Point> iter = _points.iterator();
+        while (iter.hasNext()) {
+            Point p = iter.next();
+            if (p.y > 1880) {
+                iter.remove();
+                Log.d(TAG, "point missed");
+                // TODO Success
             }
         }
 
         // 2. Check miscellaneous events like death:
 
-        if (livesLeft == 0) {
-            state = GameState.GameOver;
-        }
+        //if (livesLeft == 0) {
+        //    state = GameState.GameOver;
+        //}
 
 
         // 3. Call individual update() methods here.
         // This is where all the game updates happen.
         // For example, robot.update();
+
+        _tick = (_tick + 1) % 30;
+    }
+
+    private void hitLane(int from, int to) {
+        boolean hasHit = false;
+        Iterator<Point> iter = _points.iterator();
+        while (iter.hasNext()) {
+            Point p = iter.next();
+            if (p.x > from && p.x < to) {
+                if (p.y > 1750) {
+                    iter.remove();
+                    hasHit = true;
+                    Log.d(TAG, "point hit");
+                    // TODO failure
+                }
+            }
+        }
+        if (!hasHit) {
+            Log.d(TAG, "no point pressed");
+            // TODO failure
+        }
+    }
+
+    private void movePoints() {
+        for(Point p: _points) {
+            p.y += 10;
+        }
+    }
+
+    private void spawnPoints() {
+        if (_tick == 0) {
+            if (_rand.nextFloat() < 0.4) {
+                _points.add(new Point(_gameWidth / 3 / 2, 50));
+            }
+            if (_rand.nextFloat() < 0.4) {
+                _points.add(new Point(_gameWidth / 2, 50));
+            }
+            if (_rand.nextFloat() < 0.4) {
+                _points.add(new Point(_gameWidth - _gameWidth / 3 / 2, 50));
+            }
+        }
     }
 
     private void updatePaused(List<TouchEvent> touchEvents) {
@@ -121,7 +183,7 @@ public class GameScreen extends Screen {
         for (int i = 0; i < len; i++) {
             TouchEvent event = touchEvents.get(i);
             if (event.type == TouchEvent.TOUCH_UP) {
-
+                resume();
             }
         }
     }
@@ -134,7 +196,7 @@ public class GameScreen extends Screen {
                 if (event.x > 300 && event.x < 980 && event.y > 100
                         && event.y < 500) {
                     nullify();
-                    // game.setScreen(new MainMenuScreen(game)); TODO mainmenu
+                    // game.setScreen(new MainMenuScreen(game)); TODO mainmenu, highscore update
                     return;
                 }
             }
@@ -149,8 +211,12 @@ public class GameScreen extends Screen {
         // First draw the game elements.
 
         // Example:
-        g.drawImage(Assets.background, 0, 0, 0, 0, Assets.background.getWidth(), Assets.background.getHeight());
+        g.drawImage(Assets.background, 0, 0);
         // g.drawImage(Assets.character, characterX, characterY);
+
+        for (Point p: _points) {
+            g.drawImage(Assets.ball, p.x - 90, p.y - 90);
+        }
 
         // Secondly, draw the UI above the game elements.
         if (state == GameState.Ready)
@@ -178,8 +244,7 @@ public class GameScreen extends Screen {
         Graphics g = game.getGraphics();
 
         g.drawARGB(155, 0, 0, 0);
-        g.drawString("Tap each side of the screen to move in that direction.",
-                640, 300, paint);
+        g.drawString(Integer.toString(_score), 640, 300, paint);
 
     }
 
@@ -211,7 +276,8 @@ public class GameScreen extends Screen {
 
     @Override
     public void resume() {
-
+        if (state == GameState.Paused)
+            state = GameState.Running;
     }
 
     @Override
