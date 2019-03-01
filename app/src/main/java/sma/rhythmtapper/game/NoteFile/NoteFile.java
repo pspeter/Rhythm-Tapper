1b84e9a01931473023f75ba198a410c3090176e8
KYHSGeekCode <jourhyang123@gmail.com>
2019-03-01 11:15:14 +0900

    Added notefile

---
+++ app/src/main/java/sma/rhythmtapper/game/NoteFile/NoteFile.java
@@ -0,0 +1,20 @@
+package sma.rhythmtapper.game.NoteFile;
+
+import java.io.*;
+import java.util.*;
+import sma.rhythmtapper.game.models.*;
+
+public class NoteFile
+{
+	public NoteFile(File file)
+	{
+		
+	}
+	//note form: list of balls?
+	List<Ball> lane1;
+	List<Ball> lane2;
+	List<Ball> lane3;
+	List<Ball> lane4;
+	List<Ball> lane5;
+	
+}
--- app/src/main/java/sma/rhythmtapper/game/models/Ball.java
+++ app/src/main/java/sma/rhythmtapper/game/models/Ball.java
@@ -1,23 +1,28 @@
 package sma.rhythmtapper.game.models;
+import java.util.*;
 
 public class Ball {
     public enum BallType {
-        Normal, OneUp, Multiplier, Speeder, Bomb, Skull
+        Normal, OneUp, Multiplier, Speeder, Bomb, Skull,LongDown,LongUp,FlickLeft,FlickRight
     }
 
     public int x;
     public int y;
+	int origx;
     public BallType type;
     private double speedMultiplier;
-
+	Random random=new Random();
     public Ball(int x, int y, BallType type){
         this.x = x;
         this.y = y;
+		this.origx=x;
         this.type = type;
         this.speedMultiplier = type == BallType.Speeder ? 1.4 : 1;
     }
 
     public void update(int speed) {
+		//todo add fake move
         this.y += speed * speedMultiplier;
+		this.x = origx+(int)(50*Math.sin(y));
     }
 }
